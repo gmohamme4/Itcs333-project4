0e0832f13b947e7563ef6abeab8513396d39de50
Aysha Arif Aloraifi <aysha.aloraifi1@gmail.com>
2025-05-14 23:07:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+$users_file = 'users.txt';
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+
+    if ($username && $password) {
+        $users = file_exists($users_file) ? file($users_file, FILE_IGNORE_NEW_LINES) : [];
+        foreach ($users as $user) {
+            list($existingUser) = explode('|', $user);
+            if ($username === $existingUser) {
+                $message = "Username already exists!";
+                break;
+            }
+        }
+
+        if (!$message) {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            file_put_contents($users_file, "$username|$hashed_password\n", FILE_APPEND);
+            $message = "Registration successful! <a href='login.php'>Login here</a>";
+        }
+    } else {
+        $message = "Please fill in both fields.";
+    }
+}
+?>
+
+<h2>Register</h2>
+<form method="post">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <button type="submit">Register</button>
+</form>
+<p><?php echo $message; ?></p>
+<p>Already have an account? <a href="login.php">Login here</a></p>
