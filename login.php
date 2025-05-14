3ca56ab88aa311aa5484e6a73662057fda0d876f
Aysha Arif Aloraifi <aysha.aloraifi1@gmail.com>
2025-05-14 23:06:17 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+$users_file = 'users.txt';
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+
+    if ($username && $password) {
+        $users = file_exists($users_file) ? file($users_file, FILE_IGNORE_NEW_LINES) : [];
+        foreach ($users as $user) {
+            list($storedUser, $storedHash) = explode('|', $user);
+            if ($username === $storedUser && password_verify($password, $storedHash)) {
+                $_SESSION['username'] = $username;
+                header("Location: home.php");
+                exit;
+            }
+        }
+        $message = "Invalid username or password.";
+    } else {
+        $message = "Please fill in both fields.";
+    }
+}
+?>
+
+<h2>Login</h2>
+<form method="post">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <button type="submit">Login</button>
+</form>
+<p><?php echo $message; ?></p>
+<p>Don't have an account? <a href="index.php">Register here</a></p>
